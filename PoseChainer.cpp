29aab18e785b1087ae65627c2ca88346870e40c2
Ahmed Elsherif <ahmeds2000x@gmail.com>
2017-07-14 19:32:20 +0200

    quite a lot changed

--- PoseChainer.cpp
+++ PoseChainer.cpp
@@ -1,5 +1,7 @@
 #include "PoseChainer.h"
-#include "Arduino.h"
+#include <stdio.h>
+
+//#include "Arduino.h"
 
 /*******************************************************/
 /*******************************************************/
@@ -7,244 +9,135 @@
 /*******************************************************/
 /*******************************************************/
 /*******************************************************/
-
-//CONSTRUCTOR FOR PoseChainManager//
-PoseChainManager::PoseChainManager(long CHAIN_TIME_OUT, long RECORD_TIME_OUT)
-{
+PoseChainer::PoseChainer(long CHAIN_TIME_OUT, long RECORD_TIME_OUT) {
     _num_of_chains = 0;
-    _in_pose = -1;
-    _last_bingo_time = millis();
+    _last_bingo_time = 0;
     _BINGO_TIME_OUT = CHAIN_TIME_OUT;
     _RECORD_TIME_OUT = RECORD_TIME_OUT;
     _allowed = true;
+    for (int i = 0; i < _record_size; i++) {
+        _chain_record[i] = 0;
+    }
 
 }
 
-//ADD A PoseChain TO PoseChainManager//
-void PoseChainManager::addPoseChain(int pose_chain[], int chain_size)
-{
 
-    _chains[_num_of_chains] = PoseChain(pose_chain, chain_size);
-    _chains[_num_of_chains].printDetails();
+void PoseChainer::addChain(int *pose_chain, int chain_size) {
+    _chains[_num_of_chains] = Chain(pose_chain, chain_size);
     _num_of_chains += 1;
 }
 
-//INSERT POSE TO POSE CHAIN//
-int PoseChainManager::insertToPose(int pose_number)
-{
-    int vibration = 0;
 
-    if(pose_number > 1 && pose_number < 9 && _allowed) {
-
-        if (_in_pose == -1) {
-            whichPoseChain(pose_number);
-
-        }
-        if (_in_pose != -1) {
-            vibration = _chains[_in_pose].insertPoseToRecord(pose_number);
-        }
-    }
-
-    return vibration;
+void PoseChainer::insertPoseToChainRecord(int pose_number) {
+    pushRecordBackwards(_record_size, _chain_record, 1);
+    insertToRecord(_record_size, _chain_record, pose_number);
 }
 
-//CHECK TIMEOUTS//***HAS TO BE CALLED FROM SKETCH IN LOOP***//
-void PoseChainManager::checkWithTime()
-{
-    if (_in_pose != -1)
-    {
-        if (millis() > _chains[_in_pose].getLastInsertTime() + _RECORD_TIME_OUT)
-        {
-            _chains[_in_pose].emptyRecord();
-            _in_pose = -1;
-        }
-    }
-    if(millis() > _last_bingo_time + _BINGO_TIME_OUT){
-        _allowed = true;
-    }
-    else {
-        _allowed = false;
-    }
-}
 
-//CHECK IF A PoseChain BINGOS//
-int PoseChainManager::poseManagerBingo()
-{
-    bool bingo = false;
-    if (_in_pose != -1)
-    {
-        bingo = _chains[_in_pose].poseChainBingo();
-    }
+Chain::Chain() {}
 
-    if (bingo){
-        _last_bingo_time = millis();
-        return _in_pose+1;
 
-    }
-    else {
-        return 0;
-    }
-}
-
-//CHECK IN WHICH PoseChain USER CURRENTLY IS//
-bool PoseChainManager::whichPoseChain(int pose_number)
-{
-    for (int i = 0; i < _num_of_chains; i++)
-    {
-
-        if (pose_number == _chains[i]._pose_chain[0])
-        {
-            _in_pose = i;
-            return true;
-        }
-    }
-    return false;
-}
-
-/*******************************************************/
-/*******************************************************/
-//--------------------PoseChain------------------------//
-/*******************************************************/
-/*******************************************************/
-/*******************************************************/
-
-//CONSTRUCTOR FOR PoseChain//
-PoseChain::PoseChain(int pose_chain[], int chain_size)
-{
+Chain::Chain(int *pose_chain, int chain_size) {
     _pose_chain_size = chain_size;
-    _pose_chain[_pose_chain_size];
-    for (int i = 0; i < _pose_chain_size; i++){
-            _pose_chain[i] = pose_chain[i];
-        }
-    _current_time = millis();
-    _last_insert_time = millis();
-
+    _pose_chain = pose_chain;
 }
 
-//EPTY CONSTRUCTOR//
-PoseChain::PoseChain() {}
+sVibeChain PoseChainer::whichChain() {
+    sVibeChain vibeChain;
+    vibeChain.vibe_level = 0;
+    vibeChain.found_chain = -1;
 
+    for (short i = 0; i < _num_of_chains; i++) {
 
-//EMPTY THE POSE RECORD//
-void PoseChain::emptyRecord()
-{
-    for (int i = 0; i < _pose_chain_size; i++){
-        _chain_record[i] = 0;
-    }
-}
+        for (short index_jump = 0; index_jump < _num_of_chains; index_jump++) {
 
+            sVibeChain tempVibeChain;
+            tempVibeChain.vibe_level = 0;
+            tempVibeChain.found_chain = -1;
 
-//POP OLDEST ITEM OF RECORD AND PUSH ALL POSES BACKWARDS//
-void PoseChain::pushRecordBackwards()
-{
-    int i = 0;
-    for ( ; i < _pose_chain_size-1; )
-    {
-        _chain_record[i] = _chain_record[i+1];
-        ++i;
-    }
-    _chain_record[i] = 0;
-}
+            for (short j = index_jump; j < _record_size; j++) {
 
+                if (_chain_record[j] != 0) {
+                    if (_chain_record[j] != _chains[i]._pose_chain[j - index_jump]) {
+                        tempVibeChain.found_chain = -1;
+                        tempVibeChain.vibe_level = 0;
+                        //break loop
+                        j = _record_size;
 
-//CHECK IF POSE IS IN REQUIRED PoseChain//
-bool PoseChain::isPoseRequired(int pose_number)
-{
-    for (int i=0 ; i < _pose_chain_size; i++)
-    {
-        if(pose_number == _pose_chain[i])
-        {
-            return true;
-        }
-    }
-    return false;
-}
+                    } else {
+                        tempVibeChain.found_chain = i;
+                        ++tempVibeChain.vibe_level;
 
+                    }
+                }
+            }
 
-//INSERT NEW POSE TO THE RECORD//
-int PoseChain::insertPoseToRecord(int pose_number)
-{
-    int vibration_mode = 0;
-
-    if(isPoseRequired(pose_number))
-    {
-        if(isEmpty())
-        {
-            vibration_mode = 2;
-        }
-        else
-        {
-            vibration_mode = 1;
-        }
-        pushRecordBackwards();
-        for (int i = _pose_chain_size-1; i >= 0; i--)
-        {
-            if(_chain_record[i] == 0)
-            {
-                _chain_record[i] = pose_number;
-                _last_insert_time = millis();
-                return vibration_mode;
+            if (tempVibeChain.vibe_level >= vibeChain.vibe_level) {
+                vibeChain.vibe_level = tempVibeChain.vibe_level;
+                vibeChain.found_chain = tempVibeChain.found_chain;
             }
         }
     }
+    return vibeChain;
+}
 
-    return vibration_mode;
+void PoseChainer::printChains() {
+    printf("Print chains:\n");
+    for (int i = 0; i < _num_of_chains; i++) {
+        for (int j = 0; j < _chains[i]._pose_chain_size; j++) {
+            printf("%d\n", _chains[i]._pose_chain[j]);
+        }
+        printf("************\n");
+    }
+}
+
+void PoseChainer::printRecord() {
+    printf("Print record:\n");
+    for (int i = 0; i < _record_size; i++) {
+        printf("%d\n", _chain_record[i]);
+    }
+    printf("************\n");
+
+}
+
+void PoseChainer::emptyRecord() {
+    gEmptyRecord(_record_size, _chain_record);
 }
 
 
-//CHECK IF POSE_CHAIN WAS MET//
-bool PoseChain::poseChainBingo()
-{
-    for(int i = 0; i < _pose_chain_size; i++)
-    {
-        if(_pose_chain[i] != _chain_record[i])
-        {
-            return false;
+bool pushRecordBackwards(int record_size, int *chain_record, bool check_if_record_is_full) {
+    if (check_if_record_is_full) {
+        for (int i = 0; i < record_size; ++i) {
+            if (chain_record[i] == 0) {
+                return 0;
+            }
+        }
+        pushRecordBackwards(record_size, chain_record, 0);
+        return 1;
+    } else {
+        int i = 0;
+        for (; i < record_size - 1;) {
+            chain_record[i] = chain_record[i + 1];
+            ++i;
+        }
+        chain_record[i] = 0;
+        return 1;
+    }
+}
+
+bool insertToRecord(int record_size, int *chain_record, int pose_number) {
+
+    for (int i = 0; i < record_size; i++) {
+        if (chain_record[i] == 0) {
+            chain_record[i] = pose_number;
+            return 0;
         }
     }
-    emptyRecord();
-    _current_time = millis();
-    return true;
-
+    return 1;
 }
 
-//PRINT CURRENT RECORD//***USE ONLY FOR DEBUGGING AND IF SERIAL IS ALREADY SET***//
-/*********************************************************************************/
-void PoseChain::printDetails()
-{
-    for (int i = 0; i < _pose_chain_size; i++)
-    {
-        Serial.println(_chain_record[i]);
+void gEmptyRecord(int record_size, int *chain_record) {
+    for (int i = 0; i < record_size; i++) {
+        chain_record[i] = 0;
     }
 }
-
-//GET FIRST POSE//
-int PoseChain::firstPose()
-{
-    return _pose_chain[0];
-}
-
-//GET LAST INSERT TIME//
-long PoseChain::getLastInsertTime()
-{
-    return _last_insert_time;
-}
-
-//CHECK IF RECORD IS EMPTY//
-bool PoseChain::isEmpty()
-{
-    for (int i = 0; i < _pose_chain_size; i++){
-        if (_chain_record != 0)
-        {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-
-
-
-
-
--- PoseChainer.h
+++ PoseChainer.h
@@ -3,73 +3,55 @@
 
 #define POSE_LIMIT 10
 #define CHAIN_LIMIT 10
-#define NULL nullptr
-
+//#define NULL nullptr
+#define RECORD_LIMIT 3
 #define POSE_TIMEOUT 4000
 
-class PoseChain
+
+bool pushRecordBackwards(int record_size, int *chain_record, bool check_if_record_is_full);
+bool insertToRecord(int record_size, int *chain_record, int pose_number);
+void gEmptyRecord(int record_size, int* chain_record);
+
+struct sVibeChain {
+    volatile short vibe_level;
+    volatile short found_chain;
+};
+
+class Chain
 {
-    public:
-        //---------//
-        //FUNCTIONS//
-        PoseChain(int pose_chain[], int chain_size);
-        PoseChain();
-        void printDetails();
-        int insertPoseToRecord(int pose_number);
-        bool poseChainBingo();
-        int firstPose();
-        void emptyRecord();
-        long getLastInsertTime();
+public:
+    Chain(int *pose_chain, int chain_size);
+    Chain();
+    int* _pose_chain;
+    int _pose_chain_size;
 
-        //---------//
-        //VARIABLES//
-        int _pose_chain[POSE_LIMIT] = {0};
-
-
-    private:
-        //---------//
-        //FUNCTIONS//
-        void pushRecordBackwards();
-        bool isPoseRequired(int pose_number);
-        bool PoseChain::isEmpty();
-
-        //---------//
-        //VARIABLES//
-        int _pose_chain_size;
-        int _chain_record[POSE_LIMIT] = {0};
-        long _current_time;
-        long _last_insert_time;
+private:
 
 };
 
-class PoseChainManager
+
+class PoseChainer
 {
-    public:
-        //---------//
-        //FUNCTIONS//
-        PoseChainManager(long CHAIN_TIME_OUT, long RECORD_TIME_OUT);
-        void addPoseChain(int pose_chain[], int chain_size);
-        int insertToPose(int pose_number);
-        int poseManagerBingo();
-        void checkWithTime();
+public:
+    void addChain(int pose_chain[], int chain_size);
+    void insertPoseToChainRecord(int pose_number);
+    sVibeChain whichChain();
+    void printChains();
+    void printRecord();
+    void emptyRecord();
 
+    PoseChainer(long CHAIN_TIME_OUT, long RECORD_TIME_OUT);
 
-    private:
-        //---------//
-        //VARIABLES//
-        int _num_of_chains;
-        int _in_pose;
-        PoseChain _chains[5];
-        long _last_bingo_time;
-        long _BINGO_TIME_OUT;
-        long _RECORD_TIME_OUT;
-        bool _allowed;
+private:
+    Chain _chains[CHAIN_LIMIT];
+    int _num_of_chains;
+    long _last_bingo_time;
+    long _BINGO_TIME_OUT;
+    long _RECORD_TIME_OUT;
+    bool _allowed;
+    int _record_size = RECORD_LIMIT;
+    int _chain_record[RECORD_LIMIT];
 
-    //---------//
-    //FUNCTIONS//
-    bool whichPoseChain(int pose_number);
 };
-
-
 
 #endif
