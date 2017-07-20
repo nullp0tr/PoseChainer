272afa8929618c4954571d2e667e24a94157d541
Ahmed Elsherif <ahmeds2000x@gmail.com>
2017-07-20 13:23:02 +0200

    breaks compatibility

--- PoseChainer.cpp
+++ PoseChainer.cpp
@@ -1,139 +1,178 @@
 #include "PoseChainer.h"
+#include <stdlib.h>
 
+/*******************************************************/
+/*******************************************************/
+//--------------------PoseChainer----------------------//
+/*******************************************************/
+/*******************************************************/
+/*******************************************************/
 
-/*******************************************************/
-/*******************************************************/
-//----------------PoseChainManager---------------------//
-/*******************************************************/
-/*******************************************************/
-/*******************************************************/
-PoseChainer::PoseChainer(long CHAIN_TIME_OUT, long RECORD_TIME_OUT) {
+//
+PoseChainer::PoseChainer(uint8_t CHAIN_LIMIT, uint8_t RECORD_LIMIT)
+{
+    _chains = (Chain *) malloc(sizeof(Chain) * CHAIN_LIMIT);
+    _chain_record = (uint8_t *) malloc(sizeof(uint8_t) * RECORD_LIMIT);
+    _record_size = RECORD_LIMIT;
     _num_of_chains = 0;
-    _last_bingo_time = 0;
-    _BINGO_TIME_OUT = CHAIN_TIME_OUT;
-    _RECORD_TIME_OUT = RECORD_TIME_OUT;
     _allowed = true;
-    for (uint8_t i = 0; i < _record_size; i++) {
+
+    for (uint8_t i = 0; i < _record_size; i++)
+    {
         _chain_record[i] = 0;
     }
-
 }
 
-
-void PoseChainer::addChain(uint8_t *pose_chain, uint8_t chain_size) {
+//
+void PoseChainer::addChain(uint8_t *pose_chain, uint8_t chain_size)
+{
     _chains[_num_of_chains] = Chain(pose_chain, chain_size);
     _num_of_chains += 1;
 }
 
-
-void PoseChainer::insertPoseToChainRecord(uint8_t pose_number) {
+//
+void PoseChainer::insertPoseToChainRecord(uint8_t pose_number)
+{
     pushRecordBackwards(_record_size, _chain_record, 1);
     insertToRecord(_record_size, _chain_record, pose_number);
 }
 
+//
+Chain::Chain()
+{}
 
-Chain::Chain() {}
-
-
-Chain::Chain(uint8_t *pose_chain, uint8_t chain_size) {
+//
+Chain::Chain(uint8_t *pose_chain, uint8_t chain_size)
+{
     _pose_chain_size = chain_size;
     _pose_chain = pose_chain;
 }
 
-
-sVibeChain PoseChainer::whichChain() {
+//
+sVibeChain PoseChainer::whichChain()
+{
     sVibeChain vibeChain;
     vibeChain.vibe_level = 0;
     vibeChain.found_chain = -1;
 
-    for (short i = 0; i < _num_of_chains; i++) {
-
-        for (short index_jump = 0; index_jump < _record_size; index_jump++) {
-
+    for (short i = 0; i < _num_of_chains; i++)
+    {
+        for (short index_jump = 0; index_jump < _record_size; index_jump++)
+        {
             sVibeChain tempVibeChain;
             tempVibeChain.vibe_level = 0;
             tempVibeChain.found_chain = -1;
 
-            for (short j = index_jump; j < _record_size; j++) {
-                if (_chain_record[j] != 0) {
-
-                    if (_chain_record[j] != _chains[i]._pose_chain[j - index_jump]) {
+            for (short j = index_jump; j < _record_size; j++)
+            {
+                if (_chain_record[j] != 0)
+                {
+                    if (_chain_record[j] != _chains[i]._pose_chain[j - index_jump])
+                    {
                         tempVibeChain.found_chain = -1;
                         tempVibeChain.vibe_level = 0;
                         //break loop
                         j = _record_size;
-
-                    } else {
+                    }
+                    else
+                    {
                         tempVibeChain.found_chain = i;
                         ++tempVibeChain.vibe_level;
-
                     }
                 }
             }
 
-            if (tempVibeChain.vibe_level >= vibeChain.vibe_level) {
+            if (tempVibeChain.vibe_level >= vibeChain.vibe_level)
+            {
                 vibeChain.vibe_level = tempVibeChain.vibe_level;
                 vibeChain.found_chain = tempVibeChain.found_chain;
             }
         }
     }
+
     return vibeChain;
 }
 
-void PoseChainer::printChains() {
-    for (uint8_t i = 0; i < _num_of_chains; i++) {
-        for (uint8_t j = 0; j < _chains[i]._pose_chain_size; j++) {
+//
+void PoseChainer::printChains()
+{
+    for (uint8_t i = 0; i < _num_of_chains; i++)
+    {
+        for (uint8_t j = 0; j < _chains[i]._pose_chain_size; j++)
+        {
             //use print func here, be it with serial or with something else
+            //example: print(_chains[i]._pose_chain[j]);
         }
     }
 }
 
-void PoseChainer::printRecord() {
-    for (uint8_t i = 0; i < _record_size; i++) {
+//
+void PoseChainer::printRecord()
+{
+    for (uint8_t i = 0; i < _record_size; i++)
+    {
         //use print func here, be it with serial or with something else
+        //example: print(_chain_record[i]);
     }
-
 }
 
-
-void PoseChainer::emptyRecord() {
+//empties chain record in a PoseChainer instance
+void PoseChainer::emptyRecord()
+{
     gEmptyRecord(_record_size, _chain_record);
 }
 
-
-bool pushRecordBackwards(uint8_t record_size, uint8_t *chain_record, bool check_if_record_is_full) {
-    if (check_if_record_is_full) {
-        for (uint8_t i = 0; i < record_size; ++i) {
-            if (chain_record[i] == 0) {
+//pops the oldest item on the chain record
+bool pushRecordBackwards(uint8_t record_size, uint8_t *chain_record, bool check_if_record_is_full)
+{
+    if (check_if_record_is_full)
+    {
+        for (uint8_t i = 0; i < record_size; ++i)
+        {
+            if (chain_record[i] == 0)
+            {
                 return 0;
             }
         }
+
         pushRecordBackwards(record_size, chain_record, 0);
         return 1;
-    } else {
+    }
+    else
+    {
         uint8_t i = 0;
-        for (; i < record_size - 1;) {
+
+        for (; i < record_size - 1;)
+        {
             chain_record[i] = chain_record[i + 1];
             ++i;
         }
+
         chain_record[i] = 0;
         return 1;
     }
 }
 
-bool insertToRecord(uint8_t record_size, uint8_t *chain_record, uint8_t pose_number) {
-
-    for (uint8_t i = 0; i < record_size; i++) {
-        if (chain_record[i] == 0) {
+//inserts a value to the chain record, or rather to the array of the chain record
+bool insertToRecord(uint8_t record_size, uint8_t *chain_record, uint8_t pose_number)
+{
+    for (uint8_t i = 0; i < record_size; i++)
+    {
+        if (chain_record[i] == 0)
+        {
             chain_record[i] = pose_number;
             return 0;
         }
     }
+
     return 1;
 }
 
-void gEmptyRecord(uint8_t record_size, uint8_t *chain_record) {
-    for (uint8_t i = 0; i < record_size; i++) {
+//empties the chain record, sets all elements of an array to 0
+void gEmptyRecord(uint8_t record_size, uint8_t *chain_record)
+{
+    for (uint8_t i = 0; i < record_size; i++)
+    {
         chain_record[i] = 0;
     }
 }
--- PoseChainer.h
+++ PoseChainer.h
@@ -1,11 +1,6 @@
 #ifndef PoseChainer_h
 #define PoseChainer_h
 
-#define POSE_LIMIT 10
-#define CHAIN_LIMIT 10
-#define RECORD_LIMIT 3
-#define POSE_TIMEOUT 4000
-
 #include <stdint.h>
 
 bool pushRecordBackwards(uint8_t record_size, uint8_t *chain_record, bool check_if_record_is_full);
@@ -14,18 +9,21 @@ bool insertToRecord(uint8_t record_size, uint8_t *chain_record, uint8_t pose_num
 
 void gEmptyRecord(uint8_t record_size, uint8_t *chain_record);
 
-struct sVibeChain {
+struct sVibeChain
+{
     volatile uint8_t vibe_level;
     volatile int8_t found_chain;
 };
 
-class Chain {
+class Chain
+{
 public:
     Chain(uint8_t *pose_chain, uint8_t chain_size);
 
     Chain();
 
     uint8_t *_pose_chain;
+
     uint8_t _pose_chain_size;
 
 private:
@@ -33,7 +31,8 @@ private:
 };
 
 
-class PoseChainer {
+class PoseChainer
+{
 public:
     void addChain(uint8_t pose_chain[], uint8_t chain_size);
 
@@ -47,18 +46,18 @@ public:
 
     void emptyRecord();
 
-    PoseChainer(long CHAIN_TIME_OUT, long RECORD_TIME_OUT);
+    PoseChainer(uint8_t CHAIN_LIMIT, uint8_t RECORD_LIMIT);
 
 private:
-    Chain _chains[CHAIN_LIMIT];
     uint8_t _num_of_chains;
-    long _last_bingo_time;
-    long _BINGO_TIME_OUT;
-    long _RECORD_TIME_OUT;
-    bool _allowed;
-    uint8_t _record_size = RECORD_LIMIT;
-    uint8_t _chain_record[RECORD_LIMIT];
 
+    bool _allowed;
+
+    uint8_t _record_size;
+
+    uint8_t *_chain_record;
+
+    Chain *_chains;
 };
 
 #endif
